d3211e9c3b89b4b382a24887ef4b649ed0493000
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-17 09:24:08 +0200

    init

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/mvc/public/css/style.css" rel="stylesheet" type="text/css">
+    <title>MVC-Search</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <?php require("../app/view/template/header.php"); ?>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>MVC-Search</h1>
+                <p>Benvenuto nella nostra pagina principale!</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <?php require("../app/view/template/footer.php"); ?>
+    </div>
+</body>
+
+<script type="text/javascript" src="/mvc/public/js/script.js"></script>
+
+</html>
