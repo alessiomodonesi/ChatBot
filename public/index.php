2aa1b00079b34d228a56dca6e143e8c99591d6d0
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-20 22:05:29 +0200

    eliminato vecchio logo

--- public/index.php
+++ public/index.php
@@ -7,10 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
     <!-- style.css -->
     <link href="/chatbot/public/css/style.css" rel="stylesheet" type="text/css">
@@ -33,8 +31,7 @@
                 <div class="row">
                     <div class="header">
                         <div class="bot-icon">
-                            <img src="/chatbot/public/img/LOGO_GROSSO.png" alt="bot image" width="60" height="60"
-                                style="margin-top: 5px;">
+                            <img src="/chatbot/public/img/logo.png" alt="bot image" width="60" height="60" style="margin-top: 5px;">
                         </div>
                         <div class="bot-name">Chat Bot</div>
                     </div>
