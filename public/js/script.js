25e360b0209f971ad3c6c049e1a09d309de37cb2
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-23 01:06:18 +0200

    vabbè il tasto copia è disabile

--- public/js/script.js
+++ public/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(() => {
         $('.conversation').append('<div class="d-flex justify-content-between">' +
             '<div class="message user-message">' + message +
             '<a class="btn btn-light copy-user ml-auto" role="button">' +
-            '<img src="/chatbot/public/img/copy.png" alt="copy image" width="25" height="25"></a>' +
+            '<img src="/chatbot/public/img/copy.png" alt="copy image" width="23" height="23"></a>' +
             '</div></div>');
         $('#message-input').val('');
 
@@ -33,19 +33,19 @@ $(document).ready(() => {
                 $('.conversation').append('<div class="d-flex justify-content-between">' +
                     '<div class="message bot-message">' + response +
                     '<a class="btn btn-light copy-bot ml-auto" role="button">' +
-                    '<img src="/chatbot/public/img/copy.png" alt="copy image" width="25" height="25"></a>' +
+                    '<img src="/chatbot/public/img/copy.png" alt="copy image" width="23" height="23"></a>' +
                     '</div></div>');
                 $('.conversation').scrollTop($('.conversation')[0].scrollHeight);
             }
         });
     }
     $('.user-btn').click(() => {
-        var text = $(".user-message").text();
+        var text = $(".message user-message").text();
         console.log(text);
     });
 
     $('.bot-btn').click(() => {
-        var text = $(".bot-message").text();
+        var text = $(".message bot-message").text();
         console.log(text);
     });
 });
