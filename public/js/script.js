4db0ea87d13313b9724f4524039bd1efb7565a7b
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-22 23:55:09 +0200

    aggiunta barra di caricamento

--- public/js/script.js
+++ public/js/script.js
@@ -16,12 +16,16 @@ $(document).ready(() => {
     });
 
     function sendMessage(message) {
+        let wait = "Loading...";
+        $('.conversation').append('<div class="message bot-message loader">' + wait + '</div>');
+
         $.ajax({
             url: '/chatbot/app/controller/getResponse.php',
             method: 'POST',
             data: { message: message },
             success: (response) => {
                 // history.push(response);
+                $('.conversation .loader').remove();
                 $('.conversation').append('<div class="message bot-message">' + response + '</div>');
                 $('.conversation').scrollTop($('.conversation')[0].scrollHeight);
             }
