da2e7a7f62c1019d10d59eebb1b729e3d77286f6
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-22 18:28:08 +0200

    .

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 if (php_sapi_name() === 'cli') :
-    require __DIR__ . '/app/view/home/index.php';
+    require __DIR__ . '/app/view/cli/index.php';
 else :
     header("Location: /chatbot/public/index.php");
     exit;
