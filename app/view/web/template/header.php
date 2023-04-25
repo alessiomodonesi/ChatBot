de76b1fccb0a5628cb719969f67b80f2c6e127cb
mattia-gallinaro <gallinaromattia6@gmail.com>
2023-04-25 15:24:13 +0200

    highlight e possibilità di copiare i singoli codici
    si

--- app/view/web/template/header.php
+++ app/view/web/template/header.php
@@ -5,8 +5,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
 
 <!-- style.css -->
 <link href="/chatbot/public/css/style.css" rel="stylesheet" type="text/css">
@@ -21,3 +24,5 @@
 <!-- script.js -->
 <script type="text/javascript" src="/chatbot/public/js/script.js"></script>
 <script type="text/javascript" src="/chatbot/public/js/clipboard/dist/clipboard.min.js"></script>
+<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/styles/default.min.css">
+<script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/highlight.min.js"></script>
--- public/js/script.js
+++ public/js/script.js
@@ -1,95 +1,104 @@
 $(document).ready(() => {
+  var history = Array();
+  var count_code = 0;
+  $("#message-form").submit((event) => {
+    event.preventDefault();
+    var message = $("#message-input").val();
 
-    var history = Array();
+    if (message.trim() === "") return;
 
-    $('#message-form').submit((event) => {
-        event.preventDefault();
-        var message = $('#message-input').val();
+    history.push(message);
+    $(".conversation").append(
+      '<div class="d-flex justify-content-end">' +
+        '<div class="message user-message" id="message-' +
+        history.length +
+        '">' +
+        message +
+        '<button class="btn btn-light user-btn ml-auto" onclick="textCopy(\'' +
+        history.length +
+        "')\">" +
+        '<img src="/chatbot/public/img/copy.png" alt="copy image" width="23" height="23"></button>' +
+        "</div></div>"
+    );
+    $("#message-input").val("");
 
-        if (message.trim() === '')
-            return;
+    history.push(message);
+    sendMessage(message);
+  });
 
-        history.push(message);
-        $('.conversation').append('<div class="d-flex justify-content-end">' +
-            '<div class="message user-message" id="message-' + history.length + '">' + message +
-            '<button class="btn btn-light user-btn ml-auto" onclick="textCopy(\'' + history.length + '\')">' +
+  function sendMessage(message) {
+    let wait = "Loading...";
+    $(".conversation").append(
+      '<div class="message bot-message loader">' + wait + "</div>"
+    );
+
+    $.ajax({
+      url: "/chatbot/app/controller/getResponse.php",
+      method: "POST",
+      data: { message: message },
+      success: (response) => {
+        var message_result = showCodeInBox(response);
+        history.push(response);
+        // console.log(history);
+
+        $(".conversation .loader").remove();
+        $(".conversation").append(
+          '<div class="d-flex justify-content-start">' +
+            '<div class="message bot-message" id="message-' +
+            history.length +
+            '">' +
+            message_result +
+            '<button class="btn btn-light bot-btn ml-auto" onclick="textCopy(\'' +
+            history.length +
+            "')\">" +
             '<img src="/chatbot/public/img/copy.png" alt="copy image" width="23" height="23"></button>' +
-            '</div></div>');
-        $('#message-input').val('');
+            "</div></div>"
+        );
+        $(".conversation").scrollTop($(".conversation")[0].scrollHeight);
 
-
-        history.push(message);
-        sendMessage(message);
+        var element = document.querySelector(".user-message");
+        element = document.querySelector(".bot-message");
+        hljs.highlightAll();
+      },
     });
+  }
 
+  function showCodeInBox(message) {
+    var messages = message.split("```");
+    console.log(messages.length);
+    console.log(messages);
+    var response = "";
 
-    function sendMessage(message) {
-        let wait = "Loading...";
-        $('.conversation').append('<div class="message bot-message loader">' + wait + '</div>');
-
-        $.ajax({
-            url: '/chatbot/app/controller/getResponse.php',
-            method: 'POST',
-            data: { message: message },
-            success: (response) => {
-                var message_result = showCodeInBox(response);
-                history.push(response);
-                // console.log(history);
-
-                $('.conversation .loader').remove();
-                $('.conversation').append('<div class="d-flex justify-content-start">' +
-                    '<div class="message bot-message" id="message-' + history.length + '">' + message_result +
-                    '<button class="btn btn-light bot-btn ml-auto" onclick="textCopy(\'' + history.length + '\')">' +
-                    '<img src="/chatbot/public/img/copy.png" alt="copy image" width="23" height="23"></button>' +
-                    '</div></div>');
-                $('.conversation').scrollTop($('.conversation')[0].scrollHeight);
-
-                var element = document.querySelector('.user-message');
-                element = document.querySelector('.bot-message');
-
-                // var clipboard = new ClipboardJS("#bot"+count);
-                // clipboard.on('success', function (e) {
-                //     console.info('Action:', e.action);
-                //     console.info('Text:', e.text);
-                //     console.info('Trigger:', e.trigger);
-                //     e.clearSelection();
-                // });
-
-                // clipboard.on('error', function(e) {
-                //     console.error('Action:', e.action);
-                //     console.error('Trigger:', e.trigger);
-                // });
-            }
-        });
+    for (var i = 0; i < messages.length; i++) {
+      switch (i % 2 == 0) {
+        case true:
+          response += messages[i];
+          break;
+        case false:
+          response +=
+            '<br><button onclick="copyCodeSelected('+ count_code +')"><span>Copia</span></button><br><pre><code id="message-code-' +
+            count_code +
+            '" >' +
+            messages[i] +
+            "</code></pre></br></br>";
+          count_code++;
+          break;
+      }
     }
-
-    function showCodeInBox(message) {
-        var messages = message.split("```");
-        console.log(messages.length);
-        console.log(messages);
-        var response = "";
-
-        for (var i = 0; i < messages.length; i++) {
-            switch (i % 2 == 0) {
-                case true:
-                    response += messages[i];
-                    break;
-                case false:
-                    response += '<br /><br /><code class="language-js line-numbers" data-prismjs-copy="text-copy">' + messages[i] + '</code><br /><br />';
-                    break;
-            }
-        }
-        return response;
-    }
-
+    return response;
+  }
 });
 
 function textCopy(div_id) {
-    var testo = $('#message-' + div_id).text(); // prende il testo all'interno del div
-    // var input_tmp = $('<input>'); // crea un campo di input temporaneo
-    // $('body').append(input_tmp); // aggiunge l'input temporaneo al body della pagina
-    // input_tmp.val(testo).select(); // imposta il valore dell'input e lo seleziona
-    // document.execCommand('copy'); // copia il testo selezionato nella clipboard
-    navigator.clipboard.writeText(testo);// copia il testo del div selezionato nella clipboard
-    // input_tmp.remove(); // rimuove l'input temporaneo dalla pagina
+  var testo = $("#message-" + div_id).text(); // prende il testo all'interno del div
+  // var input_tmp = $('<input>'); // crea un campo di input temporaneo
+  // $('body').append(input_tmp); // aggiunge l'input temporaneo al body della pagina
+  // input_tmp.val(testo).select(); // imposta il valore dell'input e lo seleziona
+  // document.execCommand('copy'); // copia il testo selezionato nella clipboard
+  navigator.clipboard.writeText(testo); // copia il testo del div selezionato nella clipboard
+  // input_tmp.remove(); // rimuove l'input temporaneo dalla pagina
+}
+
+function copyCodeSelected(code_id){
+    navigator.clipboard.writeText(document.getElementById('message-code-' + code_id).textContent);
 }
