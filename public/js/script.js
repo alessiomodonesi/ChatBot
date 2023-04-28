bd17745adceeebb7f0d55bb4f0586100ab5f9330
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-28 09:51:31 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(() => {
-  var history = Array();
-  var count_code = 0;
+var history = Array();
+var count_code = 0;
 
+$(document).ready(() => {
   $("#message-form").submit((event) => {
     event.preventDefault();
     var message = $("#message-input").val();
@@ -21,71 +21,70 @@ $(document).ready(() => {
     history.push(message);
     sendMessage(message);
   });
-
-  function sendMessage(message) {
-    let wait = "Loading...";
-    $(".conversation").append(
-      '<div class="message bot-message loader">' + wait + "</div>"
-    );
-
-    $.ajax({
-      url: "/chatbot/app/controller/getResponse.php",
-      method: "POST",
-      data: { message: message },
-      success: (response) => {
-        var message_result = showCodeInBox(response);
-        history.push(response);
-
-        $(".conversation .loader").remove();
-        $(".conversation").append(
-          '<div class="d-flex justify-content-start">' +
-          '<div class="message bot-message" id="message-' + history.length + '">' + message_result +
-          '<button class="btn btn-light copy-btn" onclick="textCopy(\'' + history.length + "')\">" +
-          '<img src="/chatbot/public/img/copia.png" alt="copy image" width="30" height="30"></button>' +
-          "</div></div>"
-        );
-
-        $(".conversation").scrollTop($(".conversation")[0].scrollHeight);
-        var element = document.querySelector(".user-message");
-        element = document.querySelector(".bot-message");
-        hljs.highlightAll();
-      },
-    });
-  }
-
-  function showCodeInBox(message) {
-    var messages = message.split("```");
-    var response = "";
-
-    for (var i = 0; i < messages.length; i++) {
-      switch (i % 2 == 0) {
-        case true:
-          response += messages[i];
-          break;
-        case false:
-          response +=
-            '<pre class="d-flex">' +
-            '<code id="message-code-' + count_code + '" >' + messages[i] + '</code>' +
-            '<button class="btn btn-light code-btn" onclick="copyCodeSelected(' + count_code + ')">' +
-            '<img src="/chatbot/public/img/code.png" alt="code image" width="30" height="30">' +
-            '</button>' +
-            '</pre>';
-          count_code++;
-          break;
-      }
-    }
-    return response;
-  }
 });
 
+function sendMessage(message) {
+  let wait = "Loading...";
+  $(".conversation").append(
+    '<div class="message bot-message loader">' + wait + "</div>"
+  );
+
+  $.ajax({
+    url: "/chatbot/app/controller/getResponse.php",
+    method: "POST",
+    data: { message: message },
+    success: (response) => {
+      var message_result = showCodeInBox(response);
+      history.push(response);
+
+      $(".conversation .loader").remove();
+      $(".conversation").append(
+        '<div class="d-flex justify-content-start">' +
+        '<div class="message bot-message" id="message-' + history.length + '">' + message_result +
+        '<button class="btn btn-light copy-btn" onclick="textCopy(\'' + history.length + "')\">" +
+        '<img src="/chatbot/public/img/copia.png" alt="copy image" width="30" height="30"></button>' +
+        "</div></div>"
+      );
+
+      $(".conversation").scrollTop($(".conversation")[0].scrollHeight);
+      var element = document.querySelector(".user-message");
+      element = document.querySelector(".bot-message");
+      hljs.highlightAll();
+    },
+  });
+}
+
+function showCodeInBox(message) {
+  var messages = message.split("```");
+  var response = "";
+
+  for (var i = 0; i < messages.length; i++) {
+    switch (i % 2 == 0) {
+      case true:
+        response += messages[i];
+        break;
+      case false:
+        response +=
+          '<pre class="d-flex">' +
+          '<code id="message-code-' + count_code + '" >' + messages[i] + '</code>' +
+          '<button class="btn btn-light code-btn" onclick="copyCodeSelected(' + count_code + ')">' +
+          '<img src="/chatbot/public/img/code.png" alt="code image" width="30" height="30">' +
+          '</button>' +
+          '</pre>';
+        count_code++;
+        break;
+    }
+  }
+  return response;
+}
 
 function copyQuestion(div_id) {
   $('#message-input').val($('#message-input').val() + $('#message-' + div_id).text());
 }
 
 function textCopy(div_id) {
-  var testo = $("#message-" + div_id).text(); // prende il testo all'interno del div
-  navigator.clipboard.writeText(testo); // copia il testo del div selezionato nella clipboard
+  var testo = $("#message-" + div_id).text();
+  navigator.clipboard.writeText(testo);
 }
 
 function copyCodeSelected(code_id) {
