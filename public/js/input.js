82863148bf194dcdbc13bbee1ab1ad0eb5900983
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-28 18:47:33 +0200

    si cazzo, i tasti vanno

--- public/js/input.js
+++ public/js/input.js
@@ -2,22 +2,20 @@ $(document).ready(() => {
     $(document).on('keydown', '#message-input', (e) => {
         // shift + enter per andare a capo
         if (e.shiftKey && e.keyCode == 13) {
-            $(this).val($(this).val() + '\n');
-            console.log("shift + enter");
+            $(this).val($('#message - input').val() + '\n');
         }
 
         // enter per invio
         else if (e.which == 13) {
             $("#send-button").click();
-            // $(this).load(location.href + "#message-input");
-            console.log("enter")
+            e.preventDefault();
         }
 
         // cmd + k per cancellare la chat
         else if (e.keyCode == 75 && e.metaKey) {
             $(".message").remove();
-            // $(this).load(location.href + "#message-input");
-            console.log("cmd + k")
+            $("#message-input").val("");
+            e.preventDefault();
         }
     });
 });
