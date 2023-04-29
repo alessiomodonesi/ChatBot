a259d8a31b6df9e2f4f30db963777f836741f4c6
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-29 09:33:06 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,15 +2,11 @@
 <html lang="en">
 
 <head>
-    <!-- header.php -->
     <?php require('../app/view/web/template/header.php'); ?>
 </head>
 
 <body>
-    <!-- index.php -->
     <?php require('../app/view/web/index.php'); ?>
-
-    <!-- footer.php -->
     <?php require('../app/view/web/template/footer.php'); ?>
 </body>
 
