bab5e06b5c4d08402146633bc3d242fed8805555
alessiomodonesi <alessiomodo2004@gmail.com>
2023-04-29 09:30:04 +0200

    sistemate functions

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,8 +1,43 @@
+// funzione per restituire html del messaggio dell'utente
+function get_user_message(history, message) {
+    var user_message = '<div class="d-flex justify-content-end">' +
+        '<div class="message user-message" id="message-' + history.length + '">' + message +
+        '<button class="btn btn-light restart-btn" onclick="copyQuestion(\'' + history.length + "')\">" +
+        '<img class="restart-img" src="/chatbot/public/img/restart.png" alt="reload image">' +
+        '</button></div></div>';
+    return user_message;
+}
+
+// funzione per restituire html del messaggio del bot
+function get_bot_message(history, result) {
+    var bot_message = '<div class="d-flex justify-content-start">' +
+        '<div class="message bot-message" id="message-' + history.length + '">' + result +
+        '<button class="btn btn-light copy-btn" onclick="textCopy(\'' + history.length + "')\">" +
+        '<img class="copy-img" src="/chatbot/public/img/copia.png" alt="copy image">' +
+        '</button></div></div>';
+    return bot_message;
+}
+
+// funzione per copiare la domanda nell'input
+function copyQuestion(div_id) {
+    $('#message-input').val($('#message-input').val() + $('#message-' + div_id).text());
+}
+
+// funzione per copiare la risposta negli appunti
+function textCopy(div_id) {
+    var testo = $("#message-" + div_id).text();
+    navigator.clipboard.writeText(testo);
+}
+
+// funzione per copiare solo il codice contenuto nella risposta
+function copyCodeSelected(code_id) {
+    navigator.clipboard.writeText(document.getElementById('message-code-' + code_id).textContent);
+}
+
 // funzione per mostrare correttamente del codice contenuto nella risposta
 function showCodeInBox(message) {
     var messages = message.split("```");
     var response = "";
-    var count_code = 0;
 
     for (var i = 0; i < messages.length; i++) {
         switch (i % 2 == 0) {
@@ -22,20 +57,4 @@ function showCodeInBox(message) {
         }
     }
     return response;
-}
-
-// funzione per copiare la domanda nell'input
-function copyQuestion(div_id) {
-    $('#message-input').val($('#message-input').val() + $('#message-' + div_id).text());
-}
-
-// funzione per copiare la risposta negli appunti
-function textCopy(div_id) {
-    var testo = $("#message-" + div_id).text();
-    navigator.clipboard.writeText(testo);
-}
-
-// funzione per copiare solo il codice contenuto nella risposta
-function copyCodeSelected(code_id) {
-    navigator.clipboard.writeText(document.getElementById('message-code-' + code_id).textContent);
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,65 +1,43 @@
 $.getScript('/chatbot/public/js/functions.js');
 $.getScript('/chatbot/public/js/input.js');
 $.getScript('/chatbot/public/js/textarea.js');
+let count_code = 0;
 
 $(document).ready(() => {
   var history = Array();
+  var wait = "Caricamento...";
 
   $("#message-form").submit((event) => {
     event.preventDefault();
     var message = $("#message-input").val();
 
-    if (message.trim() === "") return;
-    history.push(message); // serve per far funzionare il tasto restart
+    if (message.trim() === '') return;
+    history.push(message);
 
-    $(".conversation").append(
-      '<div class="d-flex justify-content-end">' +
-      '<div class="message user-message" id="message-' + history.length + '">' + message +
-      '<button class="btn btn-light restart-btn" onclick="copyQuestion(\'' + history.length + "')\">" +
-      '<img class="restart-img" src="/chatbot/public/img/restart.png" alt="reload image"></button>' +
-      "</div></div>"
-    );
-
+    $(".conversation").append(get_user_message(history, message));
     $(".conversation").animate({
-      scrollTop: $(
-        '.conversation').get(0).scrollHeight
+      scrollTop: $('.conversation').get(0).scrollHeight
     }, 1000);
 
-    $("#message-input").val("");
-    history.push(message);
-    sendMessage(message);
+    $("#message-input").val('');
+    $(".conversation").append('<div class="message loader-message loader">' + wait + '</div>');
 
-    // funzione per gestire l'invio del messaggio
-    function sendMessage(message) {
-      let wait = "Loading...";
-      $(".conversation").append(
-        '<div class="message loader-message loader">' + wait + "</div>"
-      );
+    $.ajax({
+      url: "/chatbot/app/controller/getResponse.php",
+      method: "POST",
+      data: { message: message },
+      success: (response) => {
+        var message_result = showCodeInBox(response);
+        history.push(response);
 
-      $.ajax({
-        url: "/chatbot/app/controller/getResponse.php",
-        method: "POST",
-        data: { message: message },
-        success: (response) => {
-          var message_result = showCodeInBox(response);
-          history.push(response);
+        $(".conversation .loader").remove();
+        $(".conversation").append(get_bot_message(history, message_result));
+        $(".conversation").scrollTop($(".conversation")[0].scrollHeight);
 
-          $(".conversation .loader").remove();
-          $(".conversation").append(
-            '<div class="d-flex justify-content-start">' +
-            '<div class="message bot-message" id="message-' + history.length + '">' + message_result +
-            '<button class="btn btn-light copy-btn" onclick="textCopy(\'' + history.length + "')\">" +
-            '<img class="copy-img" src="/chatbot/public/img/copia.png" alt="copy image"></button>' +
-            "</div></div>"
-          );
-
-          $(".conversation").scrollTop($(".conversation")[0].scrollHeight);
-          var element = document.querySelector(".user-message");
-          element = document.querySelector(".bot-message");
-          hljs.highlightAll();
-          count_code = 0;
-        },
-      });
-    }
+        var element = document.querySelector(".user-message");
+        element = document.querySelector(".bot-message");
+        hljs.highlightAll();
+      },
+    });
   });
 });
