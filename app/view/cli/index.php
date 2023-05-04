8510e3dfbbc514e256362ce4643fa07d8d83f2e6
alessiomodonesi <alessiomodo2004@gmail.com>
2023-05-04 14:48:17 +0200

    aggiunto filtro anche su cli

--- app/view/cli/index.php
+++ app/view/cli/index.php
@@ -1,21 +1,27 @@
 <?php
 
 require(getcwd() . "/app/model/chatbot.php");
+require(getcwd() . "/app/model/filter.php");
 require(getcwd() . "/app/model/functions.php");
 
 $chatbot = new ChatBot();
+$filter = new Filter();
 $history = array();
 
 while (true) :
     $input = setInput();
-    $message = [
-        [
-            "role" => "user",
-            "content" => $input
-        ]
-    ];
 
-    $response = indent($chatbot->requestResponse($message));
+    if ($filter->checkString($input)) {
+        $response = "Ehy, il messaggio inserito non è consentito";
+    } else {
+        $message = [
+            [
+                "role" => "user",
+                "content" => $input
+            ]
+        ];
+        $response = indent($chatbot->requestResponse($message));
+    }
     array_push($history, $input, $response);
     echo "Bot: " . $response . "\n";
 endwhile;
